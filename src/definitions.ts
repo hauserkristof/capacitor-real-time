10a023d265bf042797e586e6487ab737984bc55e
Kristóf Hauser <kristof.hauser@imm-data.com>
2023-12-05 22:45:42 +0100

    feat: Add new methods to CapacitorRealTimePlugin interface
    This commit adds three new methods to the CapacitorRealTimePlugin interface: getUptime, getGnssTime, and getNetworkTime. These methods return promises with specific data types.

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,7 @@
+// definitions.ts
 export interface CapacitorRealTimePlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
+  getUptime(): Promise<{ uptime: number }>;
+  getGnssTime(): Promise<{ gnssTime: number | null }>;
+  getNetworkTime(): Promise<{ networkTime: number | null }>;
 }
