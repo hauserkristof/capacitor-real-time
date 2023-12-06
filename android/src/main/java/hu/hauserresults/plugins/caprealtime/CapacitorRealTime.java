1a5b02c16bf98717c644e1825525b2f98d2d9cc6
Kristóf Hauser <kristof.hauser@imm-data.com>
2023-12-06 15:36:53 +0100

    feat: Add error handling for unavailable time sources on Android
    This commit adds error handling for cases where the GNSS Time or Network Time is not available. Instead of throwing an exception, the code now logs an error message and rejects the corresponding plugin call with an appropriate error message. Additionally, if there is an error getting the time, a value of -1 is returned instead of null.
    
    - Added null checks and logging for GNSS Time and Network Time availability
    - Rejected plugin calls with appropriate error messages when time is not available
    - Returned -1 instead of null in case of errors

--- android/src/main/java/hu/hauserresults/plugins/caprealtime/CapacitorRealTime.java
+++ android/src/main/java/hu/hauserresults/plugins/caprealtime/CapacitorRealTime.java
@@ -19,27 +19,39 @@ public class CapacitorRealTime {
 
     public Clock getGnssTime() {
         try {
-            Clock gnssTime = SystemClock.currentGnssTimeClock();
+            Clock gnssTime = null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                gnssTime = SystemClock.currentGnssTimeClock();
+            }
             if (gnssTime == null) {
-                throw new UnsupportedOperationException("GNSS Time is not available");
+                //throw new UnsupportedOperationException("GNSS Time is not available");
+                Log.e("CapacitorRealTime", "GNSS Time is not available");
+                //call.reject("GNSS Time is not available");
             }
             return gnssTime;
         } catch (Exception e) {
             Log.e("CapacitorRealTime", "Error getting GNSS Time: " + e.getMessage());
-            return null; // Or handle the error as appropriate for your application
+            //call.reject("Error getting GNSS Time: " + e.getMessage());
+            return -1; // Or handle the error as appropriate for your application
         }
     }
 
     public Clock getNetworkTime() {
         try {
-            Clock networkTime = SystemClock.currentNetworkTimeClock();
+            Clock networkTime = null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+                networkTime = SystemClock.currentNetworkTimeClock();
+            }
             if (networkTime == null) {
-                throw new UnsupportedOperationException("Network Time is not available");
+                //throw new UnsupportedOperationException("Network Time is not available");
+                //call.reject("Network Time is not available");
+                Log.e("CapacitorRealTime", "Network Time is not available");
             }
             return networkTime;
         } catch (Exception e) {
             Log.e("CapacitorRealTime", "Error getting Network Time: " + e.getMessage());
-            return null; // Or handle the error as appropriate for your application
+            //call.reject("Error getting Network Time: " + e.getMessage());
+            return -1; // Or handle the error as appropriate for your application
         }
     }
 }
