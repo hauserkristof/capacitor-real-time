70f477151c6495ac9cbdfbdde99d1a0d8e188f45
Kristóf Hauser <hauser.kristof@gmail.com>
2025-06-01 17:53:43 +0200

    fix: ios for real

--- ios/Sources/CapacitorRealTimePlugin/CapacitorRealTime.swift
+++ ios/Sources/CapacitorRealTimePlugin/CapacitorRealTime.swift
@@ -14,22 +14,4 @@ import Kronos
     @objc public func getUptime() -> TimeInterval {
         return ProcessInfo.processInfo.systemUptime * 1000
     }
-
-    @objc public func getGnssTime() -> TimeInterval {
-        return 0
-    }
-
-    @objc public func getNetworkTime() -> TimeInterval {
-        return 0
-    }
-
-    @objc public func getTrueTime() -> TimeInterval {
-        Clock.sync {
-            time, error in
-            if let time = time {
-                return time.timeIntervalSince1970 * 1000
-            }
-            return 0
-        }
-    }
 }
